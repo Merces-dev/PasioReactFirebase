f7bd4b81859e0ee85cb51f30d3e5f1161b403767
Merces-dev <giovanitjs013@gmail.com>
2021-04-28 03:35:16 -0300

    Edit da Madruga - Estilizando Login e Cadastro

--- pasio/src/pages/cadastro/index.jsx
+++ pasio/src/pages/cadastro/index.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import Header from '../../components/header'
 import Footer from '../../components/footer'
-import { Form, Button } from 'react-bootstrap';
-
+import { Container, Form, Button } from 'react-bootstrap';
+import Logo from '../../utils/img/login.svg';
 import './index.css'
-import Interview from '../../utils/img/Interview.svg'
-import Icon05 from '../../utils/img/information.svg'
+
 
 
 const Cadastro = () => {
@@ -13,15 +12,61 @@ const Cadastro = () => {
   return (
     <div >
       <Header />
-      <main>
-        <div className="groupCadastro columnCadastro width85">
-            
+      <main className='fundo'>
+        <div className='CadastroContainer'>
+            <div className='logoPasioCadastro'>
+              <div className='opacity'>
+              <h3>Cadastro Profissional</h3>
 
+              </div>
 
+            </div>
+          <div className='CadastroContainerContent'>
 
+            <div className='formPasioCadastro'>
+              <Form className='form '>
+
+                <Form.Group controlId="formBasicEmail" className=''>
+                  <input class="mainCadastroInput" type="text" placeholder="&#61447;    Nome Completo" /> <br />
+                </Form.Group>
+                <Form.Group controlId='formBasicPassword' className=''>
+                  <input class="mainCadastroInput" type="text" placeholder="&#61475;    Email" /> <br />
+                </Form.Group>
+                <Form.Group controlId='formBasicPassword' className=''>
+                  <input class="mainCadastroInput" type="password" placeholder="&#61475;    Senha" /> <br />
+                </Form.Group>                
+                <Form.Group controlId='formBasicPassword' className=''>
+                  <input class="mainCadastroInput" type="text" placeholder="&#61475;    11 99999999" /> <br />
+                </Form.Group>              
+                <Form.Group controlId='formBasicPassword' className=''>
+                  <input class="mainCadastroInput" type="text" placeholder="&#61475;    Estado" /> <br />
+                </Form.Group>  
+                <Form.Group controlId='formBasicPassword' className=''>
+                  <input class="mainCadastroInput" type="text" placeholder="&#61475;    Cidade" /> <br />
+                </Form.Group>
+                <Form.Group controlId='formBasicPassword' className=''>
+                  <input class="mainCadastroInput" type="text" placeholder="&#61475;    Escolaridade" /> <br />
+                </Form.Group>
+                <Form.Group controlId='formBasicPassword' className=''>
+                  <input class="mainCadastroInput" type="text" placeholder="&#61475;    Lingua Estrangeira" /> <br />
+                </Form.Group>
+                <Form.Group controlId='formBasicPassword' className=''>
+                  <input class="mainCadastroInput" type="text" placeholder="&#61475;    Categoria" /> <br />
+                </Form.Group>
+                <Button style={{ backgroundColor: 'white', border: 'none' }} type="submit" >
+                  <a href="" className='buttonPrincipalCadastro' >
+                    Enviar
+                                </a>
+                </Button>
+                <p className='marginTopCadastro'><a href="">Esqueci a Senha</a> /<a href="">Cadastrar</a></p>
+
+              </Form>
+            </div>
+
+          </div>
         </div>
-      </main>
 
+      </main>
       <Footer />
     </div>
   );
--- pasio/src/pages/login/index.jsx
+++ pasio/src/pages/login/index.jsx
@@ -14,31 +14,42 @@ const Login = () => {
             <Header />
             <main className='fundo'>
                 <div className='loginContainer'>
+                    <div className='txtLogin'>
+                        <div className='h3Login'>
+                            <h3>Conectar-se à Pasio</h3>
+                        </div>
 
-                    <div className='logoPasio'>
-                        <img src={Logo} alt="" />
                     </div>
-                    <div className='formPasioLogin'>
-                        <Form className='form '>
-                            <h3>Login</h3>
+                    <div className='loginContainerContent'>
 
-                            <Form.Group controlId="formBasicEmail" className=''>
-                                <input class="mainLoginInput" type="text" placeholder="&#61447;    Email" /> <br />
-                            </Form.Group>
-                            <Form.Group controlId='formBasicPassword' className=''>
-                                <input class="mainLoginInput" type="text" placeholder="&#61475;    Senha" /> <br />
-                            </Form.Group>
-                            <Button style={{ backgroundColor: 'white', border: 'none' }} type="submit" >
-                                <a href="" className='buttonPrincipalLogin' >
-                                    Enviar
-                                </a>
-                            </Button>
-                            <p className='marginTopLogin'><a href="">Esqueci a Senha</a> /<a href="">Cadastrar</a></p>
+                        <div className='logoPasio'>
+                            <img src={Logo} alt="" />
+                        </div>
+                        <div className='formPasioLogin'>
+                            <Form className='form '>
 
-                        </Form>
+                                <Form.Group controlId="formBasicEmail" className=''>
+                                    <input class="mainLoginInput" type="text" placeholder="&#61447;    Email" /> <br />
+                                </Form.Group>
+                                <Form.Group controlId='formBasicPassword' className=''>
+                                    <input class="mainLoginInput" type="text" placeholder="&#61475;    Senha" /> <br />
+                                </Form.Group>
+                                <Button className='botaoEntireLogin ' style={{ backgroundColor: 'white', border: 'none' }}  type="submit" >
+                                    <a href="" className='buttonPrincipalLogin' >
+                                        Enviar
+                                    </a>
+                                </Button>
+                                <div style={{display:'flex', justifyContent: 'center'}}>
+                                    <p className='marginTopLogin'><a href="">Esqueci a Senha</a> /<a href="">Cadastrar</a></p>
+
+                                </div>
+
+                            </Form>
+                        </div>
+
                     </div>
-
                 </div>
+
             </main>
             <Footer />
         </div>
